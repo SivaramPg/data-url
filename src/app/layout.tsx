caaffeda795b7d064bbee602a00f0cbd83909555
SivaramPg <sivarampg95@gmail.com>
2023-10-05 01:04:54 +0530

    UI/UX updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,58 @@
-import Navbar from '@/components/Navbar'
 import './globals.css'
+
 import type { Metadata } from 'next'
 import { Space_Mono } from 'next/font/google'
+
+import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
-const spaceMono = Space_Mono({ subsets: ['latin'], weight: ['400', '700'] })
+const spaceMono = Space_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  preload: true,
+  style: 'normal',
+  display: 'auto',
+})
 
 export const metadata: Metadata = {
-  title: 'Visualize Data URL',
+  title: 'Visualize Data URLs',
   description:
-    'Visualize your data URLs. Check validity of data URL & get parsed information such as content type, media type, base64 status & data content of inputted data URLs!',
+    'Preview your data URLs. Check validity of data URL & get parsed information such as content type, media type, base64 status & data content of inputted data URLs!',
   metadataBase: new URL('https://data-url.sivaramp.com'),
   alternates: { canonical: 'https://data-url.sivaramp.com' },
   twitter: {
-    title: 'Visualize Data URL',
+    title: 'Visualize Data URLs',
     description:
-      'Visualize your data URLs. Check validity of data URL & get parsed information such as content type, media type, base64 status & data content of inputted data URLs!',
-    card: 'summary_large_image',
-    images: [],
+      'Preview your data URLs. Check validity of data URL & get parsed information such as content type, media type, base64 status & data content of inputted data URLs!',
+    card: 'summary',
+    images: [
+      {
+        type: 'image/png',
+        url: '/og.png',
+        width: 800,
+        height: 800,
+        alt: 'Visualize Data URLs',
+      },
+    ],
     creator: '@SivaramPg',
   },
   openGraph: {
-    title: 'Visualize Data URL',
+    title: 'Visualize Data URLs',
     description:
-      'Visualize your data URLs. Check validity of data URL & get parsed information such as content type, media type, base64 status & data content of inputted data URLs!',
+      'Preview your data URLs. Check validity of data URL & get parsed information such as content type, media type, base64 status & data content of inputted data URLs!',
     siteName: 'Visualize Data URL',
     url: 'https://data-url.sivaramp.com',
     locale: 'en_US',
     type: 'website',
-    images: [],
+    images: [
+      {
+        type: 'image/png',
+        url: '/og.png',
+        width: 800,
+        height: 800,
+        alt: 'Visualize Data URLs',
+      },
+    ],
   },
   icons: {
     icon: '/favicon.ico',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,21 @@
 
 import { useState } from 'react'
 import parseDataUrl from 'parse-data-url'
+import { twMerge as cn } from 'tailwind-merge'
 
 export default function Home() {
   const [dataUrl, setDataUrl] = useState<string>('')
 
+  const parsedData = parseDataUrl(dataUrl)
+
   return (
-    <main className="w-full min-h-screen px-4 py-20 flex items-start justify-start">
+    <main
+      className="w-full min-h-screen px-4 py-20 pt-24 flex items-start justify-start"
+      style={{
+        background:
+          'linear-gradient(42deg,rgba(34,58,252,0.14),rgba(83,101,115,0.11))',
+      }}
+    >
       <div className="container mx-auto w-full">
         <form
           onSubmit={(e) => {
@@ -16,9 +25,13 @@ export default function Home() {
 
             setDataUrl(formData.get('dataUrl')?.toString() ?? '')
           }}
+          onReset={(e) => {
+            e.preventDefault()
+            setDataUrl('')
+          }}
         >
           <label htmlFor="dataUrl" className="block font-bold mb-2 text-2xl">
-            Data URL:
+            Input Data URL:
           </label>
           <textarea
             name="dataUrl"
@@ -30,13 +43,13 @@ export default function Home() {
           <div className="flex items-center justify-start gap-4 mt-4">
             <button
               type="submit"
-              className="px-4 py-2 rounded bg-green-500 font-bold text-black shadow-md hover:shadow-lg"
+              className="px-10 py-3 rounded bg-green-500 font-bold text-lg text-black shadow-md hover:shadow-lg"
             >
               Submit
             </button>
             <button
               type="reset"
-              className="px-4 py-2 rounded bg-red-500 font-bold text-black shadow-md hover:shadow-lg"
+              className="px-10 py-3 rounded bg-red-500 font-bold text-lg shadow-md hover:shadow-lg text-white"
             >
               Reset
             </button>
@@ -49,9 +62,22 @@ export default function Home() {
             <label htmlFor="url" className="block font-bold mb-2 text-2xl">
               Output Iframe:
             </label>
-            <div className="w-full aspect-video border-2 border-blue-200 p-2 bg-blue-50 rounded-lg shadow">
+            <div
+              className={cn(
+                'w-full aspect-video border-2 border-blue-200 p-2 bg-blue-50 rounded-lg shadow',
+                dataUrl && !parsedData ? 'border-red-200 bg-red-50' : ''
+              )}
+            >
               {!!dataUrl ? (
-                <iframe src={dataUrl} className="w-full h-full" sandbox={''} />
+                !!parsedData ? (
+                  <iframe
+                    src={dataUrl}
+                    className="w-full h-full"
+                    sandbox={''}
+                  />
+                ) : (
+                  <InvalidState />
+                )
               ) : (
                 <EmptyState />
               )}
@@ -59,16 +85,19 @@ export default function Home() {
           </div>
           <div className="w-full lg:w-1/2 lg:shrink">
             <label htmlFor="url" className="block font-bold mb-2 text-2xl">
-              Parsed Output :
+              Parsed Output JSON:
             </label>
-            <pre className="w-full aspect-video shrink break-all whitespace-pre-wrap overflow-auto border p-2 shadow rounded-lg">
+            <pre
+              className={cn(
+                'w-full aspect-video shrink break-all whitespace-pre-wrap overflow-auto border-2 border-blue-200 p-2 bg-blue-50 shadow rounded-lg',
+                dataUrl && !parsedData ? 'border-red-200 bg-red-50' : ''
+              )}
+            >
               {!!dataUrl ? (
-                JSON.stringify(
-                  !parseDataUrl(dataUrl)
-                    ? 'Invalid URL'
-                    : parseDataUrl(dataUrl),
-                  null,
-                  2
+                !!parsedData ? (
+                  JSON.stringify(parsedData, null, 2)
+                ) : (
+                  <InvalidState />
                 )
               ) : (
                 <EmptyState />
@@ -92,12 +121,34 @@ function EmptyState() {
       >
         <path
           fill="#000000"
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M6.5 1a.5.5 0 0 0 0 1c.627 0 .957.2 1.156.478C7.878 2.79 8 3.288 8 4v7c0 .712-.122 1.21-.344 1.522c-.199.278-.53.478-1.156.478a.5.5 0 0 0 0 1c.873 0 1.543-.3 1.97-.897l.03-.044l.03.044c.427.597 1.097.897 1.97.897a.5.5 0 0 0 0-1c-.627 0-.957-.2-1.156-.478C9.122 12.21 9 11.712 9 11V4c0-.712.122-1.21.344-1.522C9.543 2.2 9.874 2 10.5 2a.5.5 0 0 0 0-1c-.873 0-1.543.3-1.97.897l-.03.044l-.03-.044C8.042 1.3 7.372 1 6.5 1ZM14 5h-3V4h3a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1h-3v-1h3V5ZM6 4v1H1v5h5v1H1a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h5Z"
-          clip-rule="evenodd"
+          clipRule="evenodd"
         />
       </svg>
       <p className="font-bold md:text-lg lg:text-xl">Awaiting your input...</p>
+    </div>
+  )
+}
+
+function InvalidState() {
+  return (
+    <div className="w-full h-full flex items-center justify-center gap-4">
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 14 14"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3.14 7.13L1.27 9a2.65 2.65 0 0 0 0 3.74h0a2.65 2.65 0 0 0 3.74 0l1.11-1.11M9 9.5h1.86a2.64 2.64 0 0 0 2.64-2.64h0a2.64 2.64 0 0 0-2.64-2.64H8.22M7 .5l-.5 2m-6 1l2 1m.5-4l1 2"
+        />
+      </svg>
+      <p className="font-bold md:text-lg lg:text-xl">Invalid data url...</p>
     </div>
   )
 }
